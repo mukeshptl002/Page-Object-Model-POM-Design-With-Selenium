9ac9a8c164757f89be4a8b4c8562fca3e3261b33
mukeshP <mukeshP@TBLSYS011.tblocks.local>
2019-05-31 17:20:37 +0530

    v4 All 12 TestCases running pom

--- src/test/java/com/crm/qa/testcases/LoginPageTest.java
+++ src/test/java/com/crm/qa/testcases/LoginPageTest.java
@@ -23,18 +23,18 @@ public class LoginPageTest extends TestBase{
 		loginPage = new LoginPage();
 	}
 	
-//	@Test(priority=1)
-//	public void loginPageTitleTest(){
-//		String title = loginPage.validateLoginPageTitle();
-//		System.out.println(title);
-//		Assert.assertEquals(title, "CRMPRO  - CRM software for customer relationship management, sales, and support.");
-//	}
-//	
-//	@Test(priority=2)
-//	public void crmLogoImageTest(){
-//		boolean flag = loginPage.validateCRMImage();
-//		Assert.assertTrue(flag);		
-//	}
+	@Test(priority=1)
+	public void loginPageTitleTest(){
+		String title = loginPage.validateLoginPageTitle();
+		System.out.println(title);
+		Assert.assertEquals(title, "CRMPRO - CRM software for customer relationship management, sales, and support.");
+	}
+	
+	@Test(priority=2)
+	public void crmLogoImageTest(){
+		boolean flag = loginPage.validateCRMImage();
+		Assert.assertTrue(flag);		
+	}
 	
 	@Test(priority=3)
 	public void loginTest(){
